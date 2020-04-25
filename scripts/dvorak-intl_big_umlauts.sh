aa9b13bdabc521b950d3797148ff3efae670c958
Gordin <9ordin@gmail.com>
2020-04-25 21:44:55 +0200

    Adds dvorak script for big Umlauts

---
+++ scripts/dvorak-intl_big_umlauts.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xmodmap -e "keycode  25 = comma less comma less adiaeresis Adiaeresis adiaeresis"
+xmodmap -e "keycode  28 = y Y y Y udiaeresis Udiaeresis udiaeresis"
+xmodmap -e "keycode  53 = q Q q Q odiaeresis Odiaeresis odiaeresis"
