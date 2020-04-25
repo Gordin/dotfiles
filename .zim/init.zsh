b11f179a74f343ddd61d4033d78a3067b9a336a2
Gordin <9ordin@gmail.com>
2020-04-25 21:50:05 +0200

    Added zim

---
+++ .zim/init.zsh
@@ -0,0 +1,14 @@
+zimfw() { source /home/gordin/.zim/zimfw.zsh "${@}" }
+fpath=(/home/gordin/.zim/modules/git/functions /home/gordin/.zim/modules/utility/functions /home/gordin/.zim/modules/git-info/functions ${fpath})
+autoload -Uz git-alias-lookup git-branch-current git-branch-delete-interactive git-dir git-ignore-add git-root git-stash-clear-interactive git-stash-recover git-submodule-move git-submodule-remove mkcd mkpw coalesce git-action git-info
+source /home/gordin/.zim/modules/environment/init.zsh
+source /home/gordin/.zim/modules/git/init.zsh
+source /home/gordin/.zim/modules/input/init.zsh
+source /home/gordin/.zim/modules/termtitle/init.zsh
+source /home/gordin/.zim/modules/utility/init.zsh
+source /home/gordin/.zim/modules/steeef/steeef.zsh-theme
+source /home/gordin/.zim/modules/zsh-completions/zsh-completions.plugin.zsh
+source /home/gordin/.zim/modules/completion/init.zsh
+source /home/gordin/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /home/gordin/.zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /home/gordin/.zim/modules/zsh-history-substring-search/zsh-history-substring-search.zsh
