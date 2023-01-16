7a34ae8fe43e5bfb12bc9c63d8e3c911d44c05a5
Gordin <9ordin@gmail.com>
2023-01-16 13:00:05 +0100

    stuff

--- .config/nvim/lua/settings/lsp.lua
+++ .config/nvim/lua/settings/lsp.lua
@@ -1,24 +1,128 @@
 local remap = vim.keymap.set
-local diagnostics = vim.diagnostic
-local lsp = vim.lsp.buf
+-- Learn to configure LSP servers, see :help lsp-zero-api-showcase
 
-local null_ls = require("null-ls")
-null_ls.setup({
-  sources = {
-    -- null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.jq,
-    null_ls.builtins.formatting.lua_format
-    -- require("null-ls").builtins.completion.spell,
+-- Reserve space for diagnostic icons
+vim.opt.signcolumn = 'yes'
+
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+  -- Replace these with whatever servers you want to install
+  'tsserver',
+  'eslint',
+  'sumneko_lua',
+  'vimls'
+})
+
+-- local cmp = require('cmp')
+-- local cmp_mappings = lsp.defaults.cmp_mappings({
+--   -- ['<C-Space>'] = cmp.mapping.complete(),
+--   -- ['<C-e>'] = cmp.mapping.abort(),
+--   ['<leader>k'] = lspbuf.signature_help,
+--   ['<leader>ca'] = lspbuf.code_action,
+-- })
+
+-- -- disable completion with tab
+-- cmp_mappings['<Tab>'] = nil
+-- cmp_mappings['<S-Tab>'] = nil
+--
+-- -- disable confirm with Enter key
+-- cmp_mappings['<CR>'] = nil
+
+-- lsp.setup_nvim_cmp({
+--   mapping = cmp_mappings
+-- })
+
+-- Pass arguments to a language server
+lsp.configure('tsserver', {
+  on_attach = function(client, bufnr)
+    print('hello tsserver')
+  end,
+  settings = {
+    completions = {
+      completeFunctionCalls = true
+    }
   }
 })
-require("mason").setup()
-require("mason-lspconfig").setup({
-  ensure_installed = { "sumneko_lua", "vimls" },
-  automatic_installation = true
+
+-- Configure lua language server for neovim
+lsp.nvim_workspace()
+
+lsp.on_attach(function(client, bufnr)
+  local bufopts = {buffer = bufnr, remap = false, silent = true}
+  local telescope = require("telescope.builtin")
+  local lspbuf = vim.lsp.buf
+
+  -- remap('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<cr>', bufopts)
+  -- remap("n", "gD",         lspbuf.declaration,                bufopts)
+  -- remap("n", "gd",         telescope.lsp_definitions,      bufopts)
+  remap("n", "gv",         function () telescope.lsp_definitions{jump_type= "vsplit"} end,      bufopts)
+  remap("n", "gt",         function () telescope.lsp_definitions{jump_type= "tab"} end,      bufopts)
+  -- remap("n", "K",          lspbuf.hover,                      bufopts)
+  -- remap("n", "gi",         telescope.lsp_implementations,  bufopts)
+  -- remap("n", "<C-k>",      lspbuf.signature_help,             bufopts)
+  remap("n", "<leader>k",  lspbuf.signature_help,          bufopts)
+  remap("n", "<leader>wa", lspbuf.add_workspace_folder,    bufopts)
+  remap("n", "<leader>wr", lspbuf.remove_workspace_folder, bufopts)
+  remap("n", "<leader>D",  telescope.lsp_type_definitions, bufopts)
+  remap("n", "<leader>rn", lspbuf.rename,                  bufopts)
+  remap("n", "<leader>ca", lspbuf.code_action,             bufopts)
+  remap("n", "gr",         telescope.lsp_references,       bufopts)
+  remap("n", "<leader>fmt",
+    function() lspbuf.format({ async = true }) end, bufopts)
+  remap("n", "<leader>wl", function()
+    print(vim.inspect(lspbuf.list_workspace_folders()))
+  end, bufopts)
+end)
+
+local lspkind = require('lspkind')
+lsp.setup_nvim_cmp({
+  formatting = {
+    -- changing the order of fields so the icon is the first
+    fields = {'abbr', 'kind'},
+    -- fields = {'menu', 'abbr', 'kind'},
+    format = lspkind.cmp_format({
+      mode = 'symbol_text', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      -- before = function (entry, vim_item)
+      --   -- ...
+      --   return vim_item
+      -- end
+    })
+  },
+  sources = {
+    { name = 'luasnip', option = { show_autosnippets = true }, priority = 9 },
+    -- For luasnip users.
+    { name = 'nvim_lua', priority = 8 },
+    { name = 'nvim_lsp', priority = 8 },
+    { name = 'path', priority = 8 },
+    {name = 'buffer', keyword_length = 3},
+  },
+  documentation = {
+    max_height = 15,
+    max_width = 60,
+    border = 'rounded',
+    col_offset = 0,
+    side_padding = 1,
+    winhighlight = 'Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None',
+    zindex = 1001
+  }
 })
 
+lsp.setup()
+
+
 -- Mappings.
+local diagnostics = vim.diagnostic
+diagnostics.config({
+  virtual_text = true,
+})
+
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
 remap("n", "<leader>di", diagnostics.open_float, opts)
@@ -26,128 +130,3 @@ remap("n", "<leader>dp", diagnostics.goto_prev,  opts)
 remap("n", "<c-j>",      diagnostics.goto_next,  opts)
 remap("n", "<leader>dn", diagnostics.goto_next,  opts)
 remap("n", "<leader>dq", diagnostics.setloclist, opts)
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-  local telescope = require("telescope.builtin")
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  remap("n", "gD",         lsp.declaration,                bufopts)
-  remap("n", "gd",         telescope.lsp_definitions,      bufopts)
-  remap("n", "gv",         function () telescope.lsp_definitions{jump_type= "vsplit"} end,      bufopts)
-  remap("n", "gt",         function () telescope.lsp_definitions{jump_type= "tab"} end,      bufopts)
-  remap("n", "K",          lsp.hover,                      bufopts)
-  remap("n", "gi",         telescope.lsp_implementations,  bufopts)
-  remap("n", "<C-k>",      lsp.signature_help,             bufopts)
-  remap("n", "<leader>k",  lsp.signature_help,             bufopts)
-  remap("n", "<leader>wa", lsp.add_workspace_folder,       bufopts)
-  remap("n", "<leader>wr", lsp.remove_workspace_folder,    bufopts)
-  remap("n", "<leader>D",  telescope.lsp_type_definitions, bufopts)
-  remap("n", "<leader>rn", lsp.rename,                     bufopts)
-  remap("n", "<leader>ca", lsp.code_action,                bufopts)
-  remap("n", "gr",         telescope.lsp_references,       bufopts)
-  remap("n", "<leader>fmt",
-    function() lsp.format({ async = true }) end, bufopts)
-  remap("n", "<leader>wl", function()
-    print(vim.inspect(lsp.list_workspace_folders()))
-  end, bufopts)
-end
-
-require("neodev").setup({})
-
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-local lspconfig = require("lspconfig")
-lspconfig.jedi_language_server.setup({
-  on_attach = on_attach,
-  flags = lsp_flags,
-  capabilities = capabilities
-})
-lspconfig.pyright.setup({
-  on_attach = on_attach,
-  capabilities = capabilities
-})
-lspconfig.tsserver.setup({
-  on_attach = on_attach,
-  capabilities = require("cmp_nvim_lsp").default_capabilities()
-})
-lspconfig.rust_analyzer.setup({
-  on_attach = on_attach,
-  settings = { ["rust-analyzer"] = {} },
-  capabilities = capabilities
-})
-lspconfig.vimls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities
-})
-
-lspconfig.bashls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities
-})
-
-lspconfig.solargraph.setup{
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings= {
-  settings = {
-    solargraph = {
-      diagnostics = true
-    }
-  }
-  }
-}
-
-lspconfig.marksman.setup({
-  on_attach = on_attach,
-  capabilities = capabilities
-})
-
-
-local json_capabilities = vim.lsp.protocol.make_client_capabilities()
-json_capabilities.textDocument.completion.completionItem.snippetSupport = true
-lspconfig.jsonls
-    .setup({ on_attach = on_attach, capabilities = json_capabilities })
-
-local runtime_path = vim.split(package.path, ";")
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-table.insert(runtime_path, os.getenv("HOME") .. '/.config/nvim/lua/?.lua')
-table.insert(runtime_path, os.getenv("HOME") .. '/.config/nvim/lua/?/?.lua')
-
-lspconfig.sumneko_lua.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      -- completion settings just to disable Text suggestions
-      completion = {
-        callSnippet = "Replace",
-        workspaceWord = true,
-        showWord = "Enable"
-      },
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = "LuaJIT",
-        path = runtime_path
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { "vim" }
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-        maxPreload = 5000,
-        preloadFileSize = 50000,
-        userThirdParty = "/home/gordin/.local/share/nvim/site/pack/packer/start"
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = { enable = false }
-    }
-  }
-})
