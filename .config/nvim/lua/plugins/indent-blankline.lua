255a40db0246f190327694be3ae4b4b40b2c1960
Gordin <9ordin@gmail.com>
2023-02-25 00:13:59 +0100

    re-configure indent-blankline

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,15 +1,13 @@
 require("indent_blankline").setup {
   buftype_exclude = {'terminal', 'nofile'},
   filetype_exclude = {'help', 'noice', 'startify', 'alpha', 'dashboard', 'packer', 'neogitstatus', 'NvimTree', 'mason.nvim'},
+  -- Setting char empty and enabling show_current_context makes it so only your current context 
+  -- has indent line
   char = ' ',
-  -- char = '▏',
-  -- char = '▕',
   show_current_context = true,
   show_current_context_start = true,  -- underline first line
+  context_char_blankline = '│',
   use_treesitter = true,
-  show_trailing_blankline_indent = false,
-  char_blankline = ' ',
-  char_list = {' ', '▏', '▏', '▏', '▏', '▏', '▏', '▏'}
-  -- char_list = {' ', '|', '¦', '┆', '┊'}
-  -- char_list_blankline = {'_', '|', '¦', '┆', '┊'}
+  show_trailing_blankline_indent = true,
+  disable_with_nolist = true,
 }
