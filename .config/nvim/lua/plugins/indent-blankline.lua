d321b1d2a8c08d33db7c0719cbf3993c80449a94
Gordin <9ordin@gmail.com>
2023-03-04 23:05:30 +0100

    change indentline highlights

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -11,3 +11,15 @@ require("indent_blankline").setup {
   show_trailing_blankline_indent = true,
   disable_with_nolist = true,
 }
+
+local utils = require("utils")
+
+utils.easyAutocmd("IndentBlankLineSpaceColor", {
+  ColorScheme = {
+    callback = function ()
+      -- Don't link to anything with background here, it will color all spaces...
+      utils.link_highlight_group('IndentBlanklineSpaceChar',          'DiagnosticSignOk')
+      utils.link_highlight_group('IndentBlanklineSpaceCharBlankline', 'DiagnosticSignOk')
+    end
+  }
+})
