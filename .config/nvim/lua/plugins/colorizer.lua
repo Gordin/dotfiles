de29f840b428100d69e1700e593caff64f1ed72f
Gordin <9ordin@gmail.com>
2023-03-09 00:43:26 +0100

    better config for colorizer

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -1,1 +1,15 @@
-require 'colorizer'.setup()
+require 'colorizer'.setup{
+  user_default_options = {
+    -- Available modes for `mode`: foreground, background,  virtualtext
+    mode = "background", -- Set the display mode.
+    -- Available methods are false / true / "normal" / "lsp" / "both"
+    -- True is same as normal
+    tailwind = "lsp", -- Enable tailwind colors
+    -- parsers can contain values used in |user_default_options|
+    sass = { enable = true, parsers = { "css" }, }, -- Enable sass colors
+    virtualtext = "■",
+    -- update color values even if buffer is not focused
+    -- example use: cmp_menu, cmp_docs
+    always_update = true
+  },
+}
