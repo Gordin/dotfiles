d23896a7cba7700dcbce000708159404cc775cbc
Gordin <9ordin@gmail.com>
2025-07-27 19:12:37 +0200

    fzf fix?

--- .config/zsh_scripts/fzf.zsh
+++ .config/zsh_scripts/fzf.zsh
@@ -1,3 +1,4 @@
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --no-ignore-vcs'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-[ -f "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh ] && source "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh
+# [ -f "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh ] && source "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh
+source <(fzf --zsh)
