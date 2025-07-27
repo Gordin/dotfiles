64fe78ca5d861e627d2bda27574152e4a8ea4d90
Gordin <9ordin@gmail.com>
2025-07-27 19:13:34 +0200

    keychain config for elchpad

--- .config/zsh_scripts/keychain.zsh
+++ .config/zsh_scripts/keychain.zsh
@@ -4,6 +4,10 @@ if [ "`hostname`" = workelch ]; then
   SSH_KEYS="$HOME/.ssh/id_rsa $HOME/.ssh/gordin_rsa $HOME/.ssh/docker_key"
   keychain -q --nogui $(echo $SSH_KEYS)
   source "$HOME/.keychain/$HOSTNAME-sh"
+elif [ "`hostname`" = elchpad ]; then
+  SSH_KEYS="$HOME/.ssh/id_ed25519"
+  keychain -q --nogui $(echo $SSH_KEYS)
+  source "$HOME/.keychain/$HOSTNAME-sh"
 else
   SSH_KEYS="$HOME/.ssh/id_ecdsa $HOME/.ssh/id_ed25519"
   keychain -q --nogui $(echo $SSH_KEYS)
